899d17adf4a2083b5cbfc462bfd50457f17ab17c
jihogrammer <jihogrammer@gmail.com>
2022-08-17 12:25:55 +0000

    feat: add CORS configuration

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,23 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://http://43.200.83.87:50983"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
